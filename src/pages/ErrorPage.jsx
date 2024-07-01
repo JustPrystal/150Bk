b2a14665735eef13ef81d4acc9637f6ade90feb4
Ali Usman <ali.usmansajid02@gmail.com>
2024-07-01 22:14:01 +0500

    errorpage and minor updates

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,16 +1,26 @@
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import BannerImage from "../assets/images/OneFifty_HomePage_v1/Banner.png"
+import Header from "../components/Header";
+
+let footerData = {
+  backgroundColor : "none"
+}
 
 let BannerData = {
   backgroundImage: BannerImage,
+  heading: "404",
+  description: "This page doesn't exist.",
+  buttonUrl: "/",
+  buttonText: "Back to Home"
 }
 
 export default function ErrorPage() {
   return (
     <>
+    <Header/>
       <Banner data={BannerData}/>
-      <Footer/>
+    <Footer data={footerData}/>
     </>
   );
 }
